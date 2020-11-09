7c47b4abe5db68707bd4144197a79854e4c00d16
MaciejMajgier <74184884+MaciejMajgier@users.noreply.github.com>
2020-11-09 12:56:24 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// nav
+const nav_button = document.querySelector('.nav_button');
+const navbar_links= document.querySelector('.navbar_links');
+
+nav_button.addEventListener('click', function(){
+    navbar_links.classList.toggle('active');
+    nav_button.classList.toggle('rotate');
+
+})
+
+//map
+
+
+
+
+
+
+
+
+
+
+
